bfc9e1b7bd1f2c61353efbc145da90b91e767b13
Yassin Alsahlani <yasnarutoa@gmail.com>
2019-05-19 10:25:42 -0700

    built surah selection logic

--- app/src/main/java/com/example/hasannaseer/quranapp2/MainActivity.java
+++ app/src/main/java/com/example/hasannaseer/quranapp2/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.hasannaseer.quranapp2;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        LinearLayout myLay = (LinearLayout) findViewById(R.id.surahSelection);
+        TextView myText = new TextView(this);
+        myText.setWidth(300);
+        myText.setHeight(50);
+        myText.setText("now its good");
+//        myText.setId(5);
+
+        myLay.addView(myText);
+
+        SurahLoader SL = new SurahLoader();
+        SL.populate(this, myLay);
+
+
     }
 }
+
+
+
+
