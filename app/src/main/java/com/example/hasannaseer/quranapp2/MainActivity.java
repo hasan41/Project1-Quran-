28647bfbe3373daa17d0be7fc2ccd8b70c7a1e44
Yassin Alsahlani <yasnarutoa@gmail.com>
2019-05-21 11:23:06 -0700

    select a surah overlay

--- app/src/main/java/com/example/hasannaseer/quranapp2/MainActivity.java
+++ app/src/main/java/com/example/hasannaseer/quranapp2/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.hasannaseer.quranapp2;
 
 import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -13,6 +15,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     ImageButton surahSelectionButton;
+    Context ctx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +24,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         surahSelectionButton = findViewById(R.id.imageButton3);
-
+        ctx = getApplication();
 
         surahSelectionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, SurahSelectionView.class);
+                Intent myIntent = new Intent(ctx, SurahSelectionView.class);
                 startActivity(myIntent);
             }
         });
 
 
+
     }
+
+//    public void selectSurah(View view){
+//        Intent myIntent = new Intent(this, SurahSelectionView.class);
+//        startActivity(myIntent);
+//    }
 }
 
 
--- app/src/main/java/com/example/hasannaseer/quranapp2/SurahSelectionView.java
+++ app/src/main/java/com/example/hasannaseer/quranapp2/SurahSelectionView.java
@@ -4,28 +4,58 @@ import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SurahSelectionView extends AppCompatActivity {
 
+
+    //static data
+    String[] surahNamesArray = {"Al-Fatihah", "Yunus", "Al-A’raf", "Al-Anfal","Al-Fatihah", "Yunus", "Al-A’raf", "Al-Anfal","Al-Fatihah", "Yunus", "Al-A’raf", "Al-Anfal"};
+    Integer[] surahLengthArray = {5, 25, 100, 158};
+
+    ListView surahList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surah_selection_view);
 
-
-        ConstraintLayout myLay = findViewById(R.id.surahSelectionView);
-        TextView myText = new TextView(this);
-        myText.setWidth(300);
-        myText.setHeight(50);
-        myText.setText("now its good");
-//        myText.setId(5);
-
-        myLay.addView(myText);
+        surahList = findViewById(R.id.surahSelectionList);
 
 
-        SurahLoader SL = new SurahLoader();
-        SL.populate(this, myLay);
+        //Getting Dimensions
+        DisplayMetrics DM = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(DM);
+        int height = DM.heightPixels;
+        int width = DM.widthPixels;
+
+        //resetting the size of the screen
+        getWindow().setLayout((int) (width * 0.8), (int) (height * 0.8));
+
+
+
+        ArrayAdapter surahArray = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, surahNamesArray);
+        surahList.setAdapter(surahArray);
+
     }
+
+
+
+    //creates the HM with the surahnames and their appropriate length
+    public Map createMap(ArrayList list1, ArrayList list2){
+        HashMap HM = new HashMap();
+        for (int x =0; x < list1.size(); x++){
+            HM.put(surahNamesArray[x], surahLengthArray[x]);
+        }
+
+        return HM;
+    }
+
 }
